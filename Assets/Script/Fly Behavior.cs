5f0ce1230a7364476e6d76763f477752a6f16bf9
Jubibani <strawberryloli3@gmail.com>
2025-07-13 16:57:29 +0800

    feat(scene): add obstacles

---
+++ Assets/Script/Fly Behavior.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlyBehavior : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _rb.linearVelocity = Vector2.up * _velocity;
+            Debug.Log("Mouse Clicked");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rb.linearVelocity.y * _rotationSpeed); ;
+    }
+
+    //private void OnCollissionEnter2D(Collision2D collision)
+    //{
+    //    GameManager.instance.GameOver();
+    //}
+}
